5df3e125ac010a311525f2beefafbddd1ece68b2
David Michel <david.michel2809@gmail.com>
2023-04-15 13:55:01 +0200

    test: refactoring Table errors tests #11

--- src/__tests__/tableErrors.test.tsx
+++ src/__tests__/tableErrors.test.tsx
@@ -5,237 +5,118 @@ import { rows, columns } from "../__mocks__";
 import { errors } from "../lib/PropsError/checkTableProps";
 import { Table } from "../lib";
 
-
-
-describe("Given i'm using incorrect Table props", () => {
-  describe("When columns props is missing", () => {
-    let expectedError: Error
-    let props: Object
-    beforeAll(() => {
-      props = { rows }
-      expectedError = new Error(errors.columns.missing)
-    })
-
-    test("Then checkTableProps should return the appropriate error", () => {
-      expect(checkTableProps(props)).toEqual(expectedError)
-    })
-
-    test("Then <Table> should display the appropriate error message", () => {
-      render(<Table {...props} />)
-      expect(screen.getByText(expectedError.message)).toBeInTheDocument()
-    })
-  })
-
-  describe("When columns type is incorrect", () => {
-    let expectedError: Error
-    let props: Object
-    beforeAll(() => {
-      expectedError = new Error(errors.columns.notArray)
-      const columns = {
+const testsCases = [
+  {
+    description: "When columns props is missing",
+    expectedError: new Error(errors.columns.missing),
+    props: { rows }
+  },
+  {
+    description: "When columns type is incorrect",
+    expectedError: new Error(errors.columns.notArray),
+    props: {
+      columns: {
         accessor: "firstName",
         title: "First name"
-      }
-      props = { columns, rows }
-    })
+      },
+      rows
+    }
+  },
+  {
+    description: "When columns elements type is incorrect",
+    expectedError: new Error(errors.columns.elements.notObject),
+    props: {
+      columns: ["2", "2", "2"],
+      rows
+    }
+  },
+  {
+    description: "When columns element have missing property",
+    expectedError: new Error(errors.columns.elements.missingProperty),
+    props: {
+      columns: [
+        columns[0],
+        { Title: "First name" }
+      ],
+      rows
+    }
+  },
+  {
+    description: "When columns element have incorrect value",
+    expectedError: new Error(errors.columns.elements.incorrectValue),
+    props: {
+      columns: [
+        columns[0],
+        { title: "Jean", accessor: 50000 }
+      ],
+      rows
+    }
+  },
+  {
+    description: "When rows props is missing",
+    expectedError: new Error(errors.rows.missing),
+    props: { columns }
+  },
+  {
+    description: "When rows type is incorrect",
+    expectedError: new Error(errors.rows.notArray),
+    props: { rows: "incorrect", columns }
+  },
+  {
+    description: "When rows elements type is incorrect",
+    expectedError: new Error(errors.rows.elements.notObject),
+    props: {
+      rows: ["2", "2", "2"],
+      columns
+    }
+  },
+  {
+    description: "When rows element have missing property",
+    expectedError: new Error(errors.rows.elements.missingProperty("lastName")),
+    props: {
+      columns,
+      rows: [
+        rows[0],
+        {
+          firstName: "Jean"
+        }
+      ]
+    }
+  },
+  {
+    description: "When rows element have incorrect value",
+    expectedError: new Error(errors.rows.elements.incorrectValue("startDate")),
+    props: {
+      columns,
+      rows: [
+        {
+          firstName: "Hanan",
+          lastName: "Jean",
+          startDate: () => "wrong",
+          department: "Marketing",
+          birthDate: "1973-12-03",
+          state: "WA",
+          street: "Ventura Blvd",
+          city: "Los Angeles",
+          zipCode: 80246
+        }
+      ]
+    }
+  }
+]
 
-    test("Then checkTableProps should return the appropriate error", () => {
-      expect(checkTableProps(props)).toEqual(expectedError)
-    })
+describe("Given i'm using incorrect Table props", () => {
+  testsCases.forEach(({ description, expectedError, props }) => {
+    describe(description, () => {
+      test("Then checkTableProps should return the appropriate error", () => {
+        expect(checkTableProps(props)).toEqual(expectedError)
+      })
 
-    test("Then <Table> should display the appropriate error message", () => {
-      render(<Table {...props} />)
-      expect(screen.getByText(expectedError.message)).toBeInTheDocument()
-    })
-  })
-
-  describe("When columns elements type is incorrect", () => {
-    let expectedError: Error
-    let props: Object
-
-    beforeAll(() => {
-      const columns = ["2", "2", "2"]
-      props = { columns, rows }
-      expectedError = new Error(errors.columns.elements.notObject)
-    })
-
-    test("Then checkTableProps should return the appropriate error", () => {
-      expect(checkTableProps(props)).toEqual(expectedError)
-    })
-
-    test("Then <Table> should display the appropriate error message", () => {
-      render(<Table {...props} />)
-      expect(screen.getByText(expectedError.message)).toBeInTheDocument()
-    })
-  })
-
-  describe("When columns element have missing property", () => {
-    let expectedError: Error
-    let props: Object
-
-    beforeAll(() => {
-      props = {
-        columns: [
-          columns[0],
-          { Title: "First name" }
-        ],
-        rows
-      }
-      expectedError = new Error(errors.columns.elements.missingProperty)
-    })
-
-    test("Then checkTableProps should return the appropriate error", () => {
-      expect(checkTableProps(props)).toEqual(expectedError)
-    })
-
-    test("Then <Table> should display the appropriate error message", () => {
-      render(<Table {...props} />)
-      expect(screen.getByText(expectedError.message)).toBeInTheDocument()
-    })
-  })
-
-  describe("When columns element have incorrect value", () => {
-    let expectedError: Error
-    let props: Object
-
-    beforeAll(() => {
-      props = {
-        columns: [
-          columns[0],
-          {
-            title: "Jean",
-            accessor: 50000
-          }
-        ],
-        rows
-      }
-      expectedError = new Error(errors.columns.elements.incorrectValue)
-    })
-
-    test("Then checkTableProps should return the appropriate error", () => {
-      expect(checkTableProps(props)).toEqual(expectedError)
-    })
-
-    test("Then <Table> should display the appropriate error message", () => {
-      render(<Table {...props} />)
-      expect(screen.getByText(expectedError.message)).toBeInTheDocument()
-    })
-  })
-
-  describe("When rows props is missing", () => {
-    let expectedError: Error
-    let props: Object
-    beforeAll(() => {
-      props = { columns }
-      expectedError = new Error(errors.rows.missing)
-    })
-
-    test("Then checkTableProps should return the appropriate error", () => {
-      expect(checkTableProps(props)).toEqual(expectedError)
-    })
-
-    test("Then <Table> should display the appropriate error message", () => {
-      render(<Table {...props} />)
-      expect(screen.getByText(expectedError.message)).toBeInTheDocument()
-    })
-  })
-
-  describe("When rows type is incorrect", () => {
-    let expectedError: Error
-    let props: Object
-    beforeAll(() => {
-      expectedError = new Error(errors.rows.notArray)
-      const rows = "incorrect"
-      props = { columns, rows }
-    })
-
-    test("Then checkTableProps should return the appropriate error", () => {
-      expect(checkTableProps(props)).toEqual(expectedError)
-    })
-
-    test("Then <Table> should display the appropriate error message", () => {
-      render(<Table {...props} />)
-      expect(screen.getByText(expectedError.message)).toBeInTheDocument()
-    })
-  })
-
-  describe("When rows elements type is incorrect", () => {
-    let expectedError: Error
-    let props: Object
-
-    beforeAll(() => {
-      const rows = ["2", "2", "2"]
-      props = { columns, rows }
-      expectedError = new Error(errors.rows.elements.notObject)
-    })
-
-    test("Then checkTableProps should return the appropriate error", () => {
-      expect(checkTableProps(props)).toEqual(expectedError)
-    })
-
-    test("Then <Table> should display the appropriate error message", () => {
-      render(<Table {...props} />)
-      expect(screen.getByText(expectedError.message)).toBeInTheDocument()
-    })
-  })
-
-  describe("When rows element have missing property", () => {
-    let expectedError: Error
-    let props: Object
-
-    beforeAll(() => {
-      props = {
-        columns,
-        rows: [
-          rows[0],
-          {
-            firstName: "Jean"
-          }
-        ]
-      }
-      expectedError = new Error(errors.rows.elements.missingProperty("lastName"))
-    })
-
-    test("Then checkTableProps should return the appropriate error", () => {
-      expect(checkTableProps(props)).toEqual(expectedError)
-    })
-
-    test("Then <Table> should display the appropriate error message", () => {
-      render(<Table {...props} />)
-      expect(screen.getByText(expectedError.message)).toBeInTheDocument()
-    })
-  })
-
-  describe("When rows element have incorrect value", () => {
-    let expectedError: Error
-    let props: Object
-
-    beforeAll(() => {
-      props = {
-        columns,
-        rows: [
-          {
-            firstName: "Hanan",
-            lastName: "Jean",
-            startDate: () => "wrong",
-            department: "Marketing",
-            birthDate: "1973-12-03",
-            state: "WA",
-            street: "Ventura Blvd",
-            city: "Los Angeles",
-            zipCode: 80246
-          }
-        ]
-      }
-      expectedError = new Error(errors.rows.elements.incorrectValue("startDate"))
-    })
-
-    test("Then checkTableProps should return the appropriate error", () => {
-      expect(checkTableProps(props)).toEqual(expectedError)
-    })
-
-    test("Then <Table> should display the appropriate error message", () => {
-      render(<Table {...props} />)
-      expect(screen.getByText(expectedError.message)).toBeInTheDocument()
+      test("Then <Table> should display the appropriate error message", () => {
+        // @ts-ignore
+        render(<Table {...props} />)
+        expect(screen.getByText(expectedError.message)).toBeInTheDocument()
+      })
     })
   })
 })
