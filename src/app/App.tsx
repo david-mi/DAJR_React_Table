ff5af6dcfcc219dc89d07909dabef6febc2d62e2
David Michel <david.michel2809@gmail.com>
2023-04-17 09:50:32 +0200

    refacto: add SelectPageSize component #17

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,13 +2,13 @@ import { columns, rows, Data } from "../__mocks__/"
 import generateEmployees from "employees-generator/generate"
 import Table from "../lib/Table"
 
-const employees = generateEmployees<Data>({ amount: 100 })
-
+const employees = generateEmployees<Data>({ amount: 1000 })
+console.log("generate")
 function App() {
   return (
     <Table<keyof Data>
       columns={columns}
-      rows={rows}
+      rows={employees}
     />
   )
 }
---
+++ src/lib/SelectPageSize/SelectPageSize.tsx
@@ -0,0 +1,23 @@
+import type { ChangeEvent } from "react"
+
+interface Props {
+  updatePageSize: ({ target }: ChangeEvent<HTMLSelectElement>) => void
+}
+
+const SelectPagesSize = ({ updatePageSize }: Props) => {
+  const pagesSizes = [10, 25, 50, 100]
+
+  return (
+    <select
+      name="pageSize"
+      id="pageSize"
+      onChange={updatePageSize}
+    >
+      {pagesSizes.map((size) => {
+        return <option key={size} value={size}>{size}</option>
+      })}
+    </select>
+  )
+}
+
+export default SelectPagesSize
